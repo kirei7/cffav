b4f320b64c3dd93f5e8f9e1c1f7541cbc3ada387
kirei7 <vlad.sereda.y@gmail.com>
2017-09-26 12:07:43 +0300

    Dropdowns in nav

--- html/parts/head.php
+++ html/parts/head.php
@@ -1,7 +1,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!--Custom styles-->
-    <link href="css/common.css" rel="stylesheet   ">
+    <link href="css/custom/common.css" rel="stylesheet   ">
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
--- html/parts/header.php
+++ html/parts/header.php
@@ -1,20 +1,58 @@
 <div class="container">
-<header>
-    <div class="text-center">
-        <img src="img/main/logo.png">
-        <h1>THE CIRCLE OF FRIENDS FOR AMERICAN VETERANS</h1>
-        <h4>American Homeless Veterans</h4>
-    </div>
-    <nav>
-        <ul class="nav nav-justified">
-            <li><a href="index.php"><span>Home</span></a></li>
-            <li><a href="what_we_do.php"><span>What we Do</span></a></li>
-            <li><a href="make_a_difference.php"><span>Make a Difference</span></a></li>
-            <li><a href="contact_us.php"><span>Contact Us</span></a></li>
-            <li><a href="meet_our_founder.php"><span>Meet our Founder</span></a></li>
-            <li><a href="careers.php"><span>Careers</span></a></li>
-            <li><a href="press_release.php"><span>Press Release</span></a></li>
-            <li><a href="donation.php"><span>Donate Now</span></a></li>
-        </ul>
-    </nav>
-</header>
+    <header>
+        <div class="text-center">
+            <img src="img/main/logo.png">
+            <h1>THE CIRCLE OF FRIENDS FOR AMERICAN VETERANS</h1>
+            <h4>American Homeless Veterans</h4>
+        </div>
+        <nav>
+            <ul class="nav nav-justified">
+                <li><a href="index.php"><span>Home</span></a></li>
+                <li id="nav-what-we-do" class="dropdown">
+                    <a href="what_we_do.php">
+                        <span>What we Do</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Our Mission</a></li>
+                        <li class="dropdown-divider"></li>
+                        <li><a href="#">Press Release</a></li>
+                        <li class="dropdown-divider"></li>
+                        <li><a href="#">Current Campaign For Veterans’ Facilities</a></li>
+                        <li class="dropdown-divider"></li>
+                        <li><a href="#">Veterans’ Bill Of Rights</a></li>
+                        <li class="dropdown-divider"></li>
+                        <li><a href="#">VBOR Affirmers</a></li>
+                    </ul>
+                </li>
+                <li id="nav-make-difference" class="dropdown">
+                    <a href="make_a_difference.php" data-toggle="dropdown" class="dropdown-toggle">
+                        <span>Make a Difference</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Sign The Veterans’ Bill Of Rights</a></li>
+                        <li class="dropdown-divider"></li>
+                        <li><a href="#">Events</a></li>
+                        <li class="dropdown-divider"></li>
+                        <li><a href="#">Current Campaign For Veterans’ Facilities</a></li>
+                    </ul>
+                </li>
+                <li><a href="contact_us.php"><span>Contact Us</span></a></li>
+                <li><a href="meet_our_founder.php"><span>Meet our Founder</span></a></li>
+                <li><a href="careers.php"><span>Careers</span></a></li>
+                <li><a href="press_release.php"><span>Press Release</span></a></li>
+                <li><a href="donation.php"><span>Donate Now</span></a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!--<ul>
+        <li class="dropdown">
+            <a data-toggle="dropdown" class="dropdown-toggle" href="#">Dropdown <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+            </ul>
+        </li>
+    </ul>-->
